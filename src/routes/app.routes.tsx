1071e63046a840afc3568d405e7500ec2f0065e4
hilquiasfmelo <hilquiasfmelo@hotmail.com>
2021-03-07 12:37:43 -0300

    Finish

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -23,6 +23,7 @@ const AppRoutes: React.FC = () => (
     <App.Screen
       options={{
         headerShown: true,
+        headerTitleAlign: 'center',
         headerTransparent: true,
         headerTitle: () => <Image source={Logo} />,
       }}
